c4ac40f8b9ee58612edd66c40526a4c2e541eb0f
Long Chu <vietlong5299@gmail.com>
2020-12-17 23:19:00 +0700

    Initialize project

---
+++ README.md
@@ -0,0 +1,1 @@
+# my-contact-spring-boot
---
+++ src/test/java/io/github/longchuviet/mycontact/MycontactApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.longchuviet.mycontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycontactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
